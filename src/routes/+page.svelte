104327b4fbf8a337cc3e617a27b612b2766f397f
Adrienfdupont <adrienf.dupont@gmail.com>
2024-07-06 15:38:48 +0200

    cleaned history display

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,13 +5,10 @@
 	import { onMount } from 'svelte';
 	import type { HistoryResearch } from '$lib/types/HistoryResearch';
 
-	let historyIsLoaded = false;
 	let historyResearches: HistoryResearch[] = [];
-	let modalHistoryResearch: HistoryResearch | null;
 
 	onMount(async () => {
 		historyResearches = getTranslationHistory();
-		historyIsLoaded = true;
 	});
 
 </script>
@@ -19,19 +16,7 @@
 <Header />
 
 <div class="overflow-y-scroll scrollbar-hidden p-2 overflow-visible">
-	{#if historyIsLoaded}
-		{#each historyResearches as historyResearch}
-				<HistoryCard {historyResearch} />
-		{/each}
-	{/if}
+	{#each historyResearches as historyResearch}
+		<HistoryCard {historyResearch} />
+	{/each}
 </div>
-
-{#if modalHistoryResearch}
-	<div class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-		<div class="bg-white p-4 rounded">
-			<p>{modalHistoryResearch.input}</p>
-			<p>{modalHistoryResearch.translation}</p>
-			<button on:mousedown={() => modalHistoryResearch = null}>Close</button>
-		</div>
-	</div>
-{/if}
