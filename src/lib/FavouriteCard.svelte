fd9f7edf706e5fa508088e4913d76081ee9521e9
Adrienfdupont <adrienf.dupont@gmail.com>
2024-07-14 20:49:59 +0200

    inline favourite info

--- src/lib/FavouriteCard.svelte
+++ src/lib/FavouriteCard.svelte
@@ -11,11 +11,10 @@
 <div class="flex justify-between flex-shrink-0 bg-primary rounded p-4 m-2"
 		 on:mousedown={extendCard} role="button" tabindex="0"
 >
-	<div class="w-1/3 whitespace-nowrap overflow-hidden text-ellipsis">{word.match}
+	<div class="whitespace-nowrap overflow-hidden text-ellipsis">{word.match}
 		<span class="font-light">({word.pronunciation})</span>
+		<span>{word.definitions}</span>
 	</div>
-
-	<div class="w-2/3 whitespace-nowrap overflow-hidden text-ellipsis">{word.definitions}</div>
 
 	<div class="flex items-center ml-2">
 		<button on:mousedown={() => {removeWordFromFavourites(word); dispatch('remove')}}>
