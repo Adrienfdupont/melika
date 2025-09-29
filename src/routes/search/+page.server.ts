098e5944e78271ea40008f932fa5326fb7912aea
Adrienfdupont <adrienf.dupont@gmail.com>
2025-09-29 22:39:10 +0200

    fix: allow app to start without Redis

--- src/routes/search/+page.server.ts
+++ src/routes/search/+page.server.ts
@@ -1,23 +1,26 @@
 import jsdom from 'jsdom';
 import translate from 'translate';
-import { createClient } from 'redis';
+import { createClient, type RedisClientType } from 'redis';
 import type { PageServerLoad } from './$types';
 import type { TranslationResult } from '$lib/types/TranslationResult';
 import type { WordData } from '$lib/types/WordData';
+import { REDIS_URL } from '$env/static/private';
 
-const client = await createClient()
-	.on('error', (err) => console.error('Redis Client Error', err))
-	.connect();
+let redisClient: RedisClientType | null = null;
 
 export const load: PageServerLoad = async ({ url }: { url: URL }): Promise<TranslationResult> => {
+	await initRedisClient();
 	const input = decodeURIComponent(url.searchParams.get('input') ?? '');
 	const translation = await translate(input, { from: 'en', to: 'fa' });
 	const wordsData: WordData[] = [];
 
 	for (const word of translation.split(' ')) {
-		const cachedWordData = await client.get(word);
-		if (cachedWordData) {
-			wordsData.push(JSON.parse(cachedWordData));
+		let wordCache: string | null = null;
+		if (redisClient) {
+			wordCache = await redisClient.get(word);
+		}
+		if (wordCache) {
+			wordsData.push(JSON.parse(wordCache));
 		} else {
 			for (let i = 0; i < 3; i++) {
 				const pattern = word.slice(0, word.length - i);
@@ -26,7 +29,9 @@ export const load: PageServerLoad = async ({ url }: { url: URL }): Promise<Trans
 				if (res.status === 200) {
 					const rawPage = await res.text();
 					const wordData = extractWordData(pattern, rawPage);
-					await client.set(word, JSON.stringify(wordData));
+					if (redisClient) {
+						await redisClient.set(word, JSON.stringify(wordData));
+					}
 					wordsData.push(wordData);
 					break;
 				}
@@ -85,4 +90,30 @@ function extractWordData(match: string, sectionContent: string): WordData {
 		}
 	}
 	return wordData;
+}
+
+async function initRedisClient(): Promise<RedisClientType | null> {
+	if (!redisClient) {
+		const connectStart = Date.now();
+		try {
+			redisClient = createClient({
+				url: REDIS_URL,
+				socket: {
+					reconnectStrategy: (retries) => {
+						const elapsed = Date.now() - connectStart;
+						if (elapsed >= 3000) {
+							return new Error('Retry time exhausted');
+						}
+						return Math.min(retries * 100, 1000);
+					}
+				}
+			});
+
+			await redisClient.connect();
+		} catch (error) {
+			console.error('Error while connecting to Redis:', error);
+			redisClient = null;
+		}
+	}
+	return redisClient;
 }
